06250c9000fd8d0ef4f3146b9a88cc05bdc27093
Unknown <Zachary.Lynch1337@gmail.com>
2017-12-21 12:46:11 -0800

    Added try exceptions for errors. Going to define main next

--- spider.py
+++ spider.py
@@ -3,6 +3,7 @@ import bs4 as bs
 import random
 import requests
 import string
+import json
 
 def starting_url(): 
 	starting = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(3))
@@ -14,3 +15,34 @@ def local_links(url, link):
 		return ''.join([url,link])
 	else: 
 		return link
+
+def get_links(url): 
+	try: 
+		resp = requests.get(url)
+		soup = bs.BeautifulSoup(resp.txt,'lxml')
+		body = soup.body
+		links = [link.get('href')for link in body.find_all('a')]
+		links = [local_links(url, link) for link in links]
+		links = [str(link.enconde('ascii'))for link in links]
+		return links
+
+	except TypeError as e: 
+		print(e)
+		print('Got a type error. Probably got a none ') 
+		return[]
+	except IndexError as e: 
+		print(e)
+		print("Didn't get anything useful, returning empty list")
+		return []
+	except AttributeError as e: 
+		print(e)
+		print('likely got none. throwing error')
+
+S
+
+
+
+
+
+
+
